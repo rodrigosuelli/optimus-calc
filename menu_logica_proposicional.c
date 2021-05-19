1891f004e0329b8d1336e5245a909f765ca7ed87
Rodrigo Suélli <rodrigosuellli@gmail.com>
2021-05-19 10:28:14 -0300

    Fix input checking in menu-logica-proposicional

--- menu_logica_proposicional.c
+++ menu_logica_proposicional.c
@@ -55,12 +55,23 @@ char negacao(unsigned char n)
     return 'V';
 }
 
-bool isValid(unsigned char userInput)
+unsigned char getInputAndCheckIsValid(char varName)
 {
-  if (userInput != 0 && userInput != 1)
-    return false;
+  int p;
+  char enterKey;
+
+  printf("Insira o valor de %c (0 ou 1):\n", varName);
+
+  if (scanf("%d%c", &p, &enterKey) != 2 || enterKey != '\n' || p != 0 && p != 1)
+  {
+    puts("Ops, entrada invalida.");
+    fflush(stdin);
+    getInputAndCheckIsValid(varName);
+  }
   else
-    return true;
+  {
+    return p;
+  }
 }
 
 void getMenuLogicaProposicionalInput()
@@ -82,65 +93,39 @@ void getMenuLogicaProposicionalInput()
 
 void handleMenuLogicaProposicionalInput(unsigned char selectedOption)
 {
-  int p, q;
+  unsigned char p, q;
+
+  if (selectedOption != 2)
+  {
+    p = getInputAndCheckIsValid('p');
+  }
+
+  if (selectedOption > 1 && selectedOption < 8)
+  {
+    q = getInputAndCheckIsValid('q');
+  }
 
   switch (selectedOption)
   {
   case 1:
-    puts("Insira o valor de p (0 ou 1):");
-    scanf("%d", &p);
-
     printf("%c", negacao(p));
     break;
   case 2:
-    puts("Insira o valor de q (0 ou 1):");
-    scanf("%d", &q);
-
     printf("%c", negacao(q));
     break;
   case 3:
-    puts("Insira o valor de p (0 ou 1):");
-    scanf("%d", &p);
-
-    puts("Insira o valor de q (0 ou 1):");
-    scanf("%d", &q);
-
     printf("%c", conjuncao(p, q));
     break;
   case 4:
-    puts("Insira o valor de p (0 ou 1):");
-    scanf("%d", &p);
-
-    puts("Insira o valor de q (0 ou 1):");
-    scanf("%d", &q);
-
     printf("%c", disjuncao(p, q));
     break;
   case 5:
-    puts("Insira o valor de p (0 ou 1):");
-    scanf("%d", &p);
-
-    puts("Insira o valor de q (0 ou 1):");
-    scanf("%d", &q);
-
     printf("%c", disjuncaoExclusiva(p, q));
     break;
   case 6:
-    puts("Insira o valor de p (0 ou 1):");
-    scanf("%d", &p);
-
-    puts("Insira o valor de q (0 ou 1):");
-    scanf("%d", &q);
-
     printf("%c", implicacao(p, q));
     break;
   case 7:
-    puts("Insira o valor de p (0 ou 1):");
-    scanf("%d", &p);
-
-    puts("Insira o valor de q (0 ou 1):");
-    scanf("%d", &q);
-
     printf("%c", equivalencia(p, q));
     break;
   case 8:
